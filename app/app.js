7c5de4a2cbfe4e60262ed2dc71285d3746064b02
Jess Hendricks <music@jesshendricks.com>
2016-03-01 16:28:53 -0500

    Added the BASE React code into app.js based on workshop 4 and 5. Note that it kills the Saved Playlist page. I'll be working on that. Eventually the React will replace all of the separate html pages.

--- app/app.js
+++ app/app.js
@@ -1,1 +1,51 @@
-/* Empty for now. :) */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { IndexRoute, Router, Route, browserHistory } from 'react-router'
+
+/**
+ * A Profile Page PLACEHOLDER
+ */
+class ProfilePage extends React.Component {
+  render() {
+    return (
+      <p>This is the profile page.</p>
+    );
+  }
+}
+
+/**
+ * The Saved Playlist Page PLACEHOLDER page.
+ */
+class SavedPlayListPage extends React.Component {
+  render() {
+      return (
+        <p>This is the playlist page.</p>
+      );
+  }
+}
+
+
+/**
+ * The primary component in our application.
+ * The Router will give it different child Components as the user clicks
+ * around the application.
+ *
+ * TODO: Add components for the sidebar AND the navbar.
+ */
+class App extends React.Component {
+  render() {
+    return (
+      <div>{this.props.children}</div>
+    )
+  }
+}
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      {/* Show the Feed at / */}
+      <IndexRoute component={SavedPlayListPage} />
+      <Route path="profile/:id" component={ProfilePage} />
+    </Route>
+  </Router>
+),document.getElementById('content-column'));
