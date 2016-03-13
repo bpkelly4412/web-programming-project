4bed4d238e9db7715b8ae5f87d6fe4c4ac85069b
mzenzieUmass <mzenzie@umass.edu>
2016-03-13 00:25:41 -0500

    initial recommendation

---
+++ app/components/recommend.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {getUserData, setUserData} from '../server';
+
+export default class Recommend extends React.Component {
+
+    constructor(props) {
+	super(props);
+	this.state = { value: "" };
+    }
+
+    refresh() {
+	getUserData(this.props.userID, (userData) => {
+	    this.setState({userInfo: userData});
+	});
+
+    }
+
+    componentDidMount() {
+	this.refresh();
+    }
+
+    handleChange(event) {
+	this.setState( {value: event.target.value} );
+    }
+
+    render() {
+	return (
+	    Hi
+      )
+    }
+}
