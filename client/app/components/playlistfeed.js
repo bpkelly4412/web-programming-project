cde234d60b8a654aec0d9b91850af43a888bb871
Jess Hendricks <music@jesshendricks.com>
2016-04-02 09:35:12 -0400

    PlayListFeed... fixed the warning for a missing key in an iterator.

--- client/app/components/playlistfeed.js
+++ client/app/components/playlistfeed.js
@@ -24,9 +24,9 @@ export default class PlayListFeed extends React.Component {
     getPlaylistFeed(this.props.userID, (feedData) => {
       this.setState(feedData);
     });
-      getUserData(this.props.userID, (userData) => {
-	  this.setState({userData: userData});
-      });
+    getUserData(this.props.userID, (userData) => {
+      this.setState({userData: userData});
+    });
   }
 
   handleNewPlaylistNameChange(e) {
@@ -54,10 +54,10 @@ export default class PlayListFeed extends React.Component {
         this.state.newPlaylistGame,
         this.state.newPlaylistGenre,
         this.state.newPlaylistDescription, () =>
-      {
-        this.refresh();
-        // this.context.router.push({pathname: "/song-list/" + newPlaylistID + "/" + this.props.userID});
-      });
+        {
+          this.refresh();
+          // this.context.router.push({pathname: "/song-list/" + newPlaylistID + "/" + this.props.userID});
+        });
 
     }
   }
@@ -75,73 +75,72 @@ export default class PlayListFeed extends React.Component {
               <div className="panel-body">
                 <div className="text-center">
                   <button type="button"
-                    className="btn btn-default playlist-button"
-                    data-toggle="collapse"
-                    data-target="#addPlaylistForm">
+                          className="btn btn-default playlist-button"
+                          data-toggle="collapse"
+                          data-target="#addPlaylistForm">
                     <span className="fa fa-plus fa-2x"></span>
                     <h3>Add New Playlist</h3>
                   </button>
                 </div>
                 <div id="addPlaylistForm" className="collapse">
-                <form>
-                  <div className="form-group col-md-4">
-                    <label htmlFor="playlistName">Name</label>
-                    <input type="text"
-                      id="playlistName"
-                      className="form-control"
-                      placeholder="Name"
-                      value={this.state.value}
-                      onChange={(e) => this.handleNewPlaylistNameChange(e)} />
-                  </div>
-                  <div className="form-group col-md-4">
-                    <label htmlFor="playlistGame">Game</label>
-                    <input type="text"
-                      id="playlistGame"
-                      className="form-control"
-                      placeholder="Game"
-                      value={this.state.value}
-                      onChange={(e) => this.handleNewPlaylistGameChange(e)} />
-                  </div>
-                  <div className="form-group col-md-4">
-                    <label htmlFor="playlistGenre">Genre</label>
-                    <input type="text"
-                      id="playlistGenre"
-                      className="form-control"
-                      placeholder="Genre"
-                      value={this.state.value}
-                      onChange={(e) => this.handleNewPlaylistGenreChange(e)} />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="playlistDescription">Description</label>
-                    <input type="text"
-                      id="playlistDescription"
-                      className="form-control"
-                      placeholder="Description"
-                      value={this.state.value}
-                      onChange={(e) => this.handleNewPlaylistDescChange(e)} />
-                  </div>
-                  <button type="submit"
-                    className="btn btn-default playlist-button"
-                    onClick={(e) => this.handleAddPlaylistClick(e)}>
-                    Submit
-                  </button>
-                </form>
+                  <form>
+                    <div className="form-group col-md-4">
+                      <label htmlFor="playlistName">Name</label>
+                      <input type="text"
+                             id="playlistName"
+                             className="form-control"
+                             placeholder="Name"
+                             value={this.state.value}
+                             onChange={(e) => this.handleNewPlaylistNameChange(e)} />
+                    </div>
+                    <div className="form-group col-md-4">
+                      <label htmlFor="playlistGame">Game</label>
+                      <input type="text"
+                             id="playlistGame"
+                             className="form-control"
+                             placeholder="Game"
+                             value={this.state.value}
+                             onChange={(e) => this.handleNewPlaylistGameChange(e)} />
+                    </div>
+                    <div className="form-group col-md-4">
+                      <label htmlFor="playlistGenre">Genre</label>
+                      <input type="text"
+                             id="playlistGenre"
+                             className="form-control"
+                             placeholder="Genre"
+                             value={this.state.value}
+                             onChange={(e) => this.handleNewPlaylistGenreChange(e)} />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="playlistDescription">Description</label>
+                      <input type="text"
+                             id="playlistDescription"
+                             className="form-control"
+                             placeholder="Description"
+                             value={this.state.value}
+                             onChange={(e) => this.handleNewPlaylistDescChange(e)} />
+                    </div>
+                    <button type="submit"
+                            className="btn btn-default playlist-button"
+                            onClick={(e) => this.handleAddPlaylistClick(e)}>
+                      Submit
+                    </button>
+                  </form>
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div className="row">
-          {this.state.contents.map((playlist) => {
-              return (
-		  <div>
-              <Playlist key={playlist._id}
-                userID={this.props.userID}
-                data={playlist}
-                plFeedID={this.state._id}
-                callbackPlaylistFeed = {this.onChildChanged} />
-	      {this.state.userData? (this.state.userData.recommend ? <Recommend userID = {this.props.userID}/> : null) : null}
-		  </div>
+          {this.state.contents.map((playlist, i) => {
+            return (
+              <div key={i}>
+                <Playlist userID={this.props.userID}
+                          data={playlist}
+                          plFeedID={this.state._id}
+                          callbackPlaylistFeed = {this.onChildChanged} />
+                {this.state.userData? (this.state.userData.recommend ? <Recommend userID = {this.props.userID}/> : null) : null}
+              </div>
             );
           })}
 
